d42dc0179354dd332eecbcf0055e4a404c44b6ac
Chengyuan Li <chengyli@ebay.com>
2021-01-10 07:20:49 -0700

    Add perf example

---
+++ perf/README.md
@@ -0,0 +1,3 @@
+# Run container
+echo -1 > /proc/sys/kernel/perf_event_paranoid
+docker run --rm -it --security-opt seccomp=unconfined registry/perf_test:v1 bash
---
+++ perf/perf_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void calculation(void)
+{
+	int i;
+	while (1) {
+		i = i + 1;
+	}
+}
+
+int main(void)
+{
+	calculation();
+	return 0;
+}
